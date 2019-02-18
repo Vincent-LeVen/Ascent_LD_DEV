5fb33744f88f03ac7af0b55e8382f9a445aeefd8
Vincent-LeVen <32410365+Vincent-LeVen@users.noreply.github.com>
2019-02-18 15:10:59 +0100

    Initial commit

---
+++ Ascent LD_Dev version/Assets/Scripts/gravUpBlock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravUpBlock : MonoBehaviour
+{
+
+    private Rigidbody myRigidbody;
+    public float gravUpForce = 18.0f;
+    public float gravUpCD = 3.0f;
+    private bool onCD = false;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T) && onCD == false)
+        {
+            onCD = true;
+            myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, gravUpForce , myRigidbody.velocity.z);
+            StartCoroutine(GravityTimeController());
+        }
+    }
+
+    IEnumerator GravityTimeController()
+    {
+        yield return new WaitForSecondsRealtime(gravUpCD);
+        onCD = false;
+        StopCoroutine(GravityTimeController());
+    }
+}
